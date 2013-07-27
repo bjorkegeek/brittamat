0bce103b613d5c50cbd64e305dbccf9aff86bee0
David Björkevik <david@bjorkevik.se>
2013-07-27 20:00:02 +0200

    Improved HTML5 formatting

--- html5.py
+++ html5.py
@@ -11,18 +11,18 @@ def shopping_list_to_html5(shopping_list, outputfile):
         print >>outputfile, u'<h1>' + escape(cat) + u'</h1>'
         print >>outputfile, u'<table class="tbl">'
         print >>outputfile, u'  <thead>'
-        print >>outputfile, u'    <tr><th colspan="2">Mängd</th><th>Ingrediens</th><th>Till</th></tr>'
+        print >>outputfile, u'    <tr><th colspan="2" class="amt">Mängd</th><th class="ingredient">Ingrediens</th><th class="sources">Till</th></tr>'
         print >>outputfile, u'  </thead>'
         print >>outputfile, u'  <tbody>'
         for ingredient in sorted(ingredients):
             print >>outputfile, u'  <tr>'
             if ingredient.quantity:
                 formatst = u"%.1f"
-                print >>outputfile,u'    <td class="numeric">' + locale.format(formatst,ingredient.quantity.magnitude) + '</td>'
-                print >>outputfile,u'    <td>' + escape(alg.translate_unit(unicode(ingredient.quantity.units))) + '</td>'
+                print >>outputfile,u'    <td class="numeric amt-magnitude">' + locale.format(formatst,ingredient.quantity.magnitude) + '</td>'
+                print >>outputfile,u'    <td class="amt-unit">' + escape(alg.translate_unit(unicode(ingredient.quantity.units))) + '</td>'
             else:
                 print >>outputfile,u'    <td colspan="2"></td>'
-            print >>outputfile,u'    <td>' + escape(ingredient.name) + '</td>'
+            print >>outputfile,u'    <td class="ingredient">' + escape(ingredient.name) + '</td>'
             print >>outputfile,u'    <td class="sources">' + escape(", ".join([x.dish.name for x in ingredient.sources])) + '</td>'
             print >>outputfile, u'  </tr>'
         print >>outputfile, u'  </tbody>'
